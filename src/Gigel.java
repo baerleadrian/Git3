ab5282dcc46e19654d5a41954dfda70e66420193
baerleadrian <adybaerle@gmail.com>
2023-02-28 18:29:28 +0200

    am mancat, nu imi mai e foame

--- src/Gigel.java
+++ src/Gigel.java
@@ -1,6 +1,6 @@
 public class Gigel {
     public static void main(String[] args) {
         System.out.println("salutare lume ");
-        System.out.println(" nu mii foame");
+        System.out.println(" nuame");
     }
 }
